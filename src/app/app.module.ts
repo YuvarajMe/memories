fc519097a8ee3dc345a076a8f3a2d7912a163109
Yuvaraj <yuvarajcse92@gmail.com>
2017-03-15 00:17:13 +0530

    created login html page and setup'ed route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { AppRoute } from './app.routes';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(AppRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by yuvaraj on 14/03/17.
+ */
+
+import { Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+
+export const AppRoute:Routes = [
+    {
+        path : '',
+        component : LoginComponent
+    }
+    ];
+
+
