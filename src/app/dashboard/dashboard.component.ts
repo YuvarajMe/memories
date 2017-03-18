bd2bdc10ee021bb6e12c34d7ac2b1016e72c999a
Yuvaraj <yuvarajcse92@gmail.com>
2017-03-18 23:57:04 +0530

    show loader when comments are loading from web service

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -39,46 +39,64 @@ export class DashboardComponent implements OnInit {
 
     toggleComment = function (memory) {
         var memoryId = memory.id;
+
+        memory.isMemoryLoading = true;
+
         if(memory.showComment == undefined){
             memory.showComment = true;
         } else {
             memory.showComment = !memory.showComment;
-
         }
 
-        memory.comments = [
-            {
-                name: 'User 1',
-                comment: 'Wow! Nice photos.'
-            },
-            {
-                name: 'User 2',
-                comment: 'It was a wonderful celebration.'
-            },
-            {
-                name: 'User 3',
-                comment: 'Wow! Nice photos.'
-            },
-            {
-                name: 'User 4',
-                comment: 'It was a wonderful celebration.'
-            },
-            {
-                name: 'User 5',
-                comment: 'Wow! Nice photos.'
-            },
-            {
-                name: 'User 6',
-                comment: 'It was a wonderful celebration.'
-            },
-            {
-                name: 'User 7',
-                comment: 'Wow! Nice photos.'
-            },
-            {
-                name: 'User 8',
-                comment: 'It was a wonderful celebration.'
-            }
-        ];
+
+        var CallMemoriesForId = function() {
+            setTimeout(function () {
+                memory.comments = [
+                    {
+                        name: 'User 1',
+                        comment: 'Wow! Nice photos.'
+                    },
+                    {
+                        name: 'User 2',
+                        comment: 'It was a wonderful celebration.'
+                    },
+                    {
+                        name: 'User 3',
+                        comment: 'Wow! Nice photos.'
+                    },
+                    {
+                        name: 'User 4',
+                        comment: 'It was a wonderful celebration.'
+                    },
+                    {
+                        name: 'User 5',
+                        comment: 'Wow! Nice photos.'
+                    },
+                    {
+                        name: 'User 6',
+                        comment: 'It was a wonderful celebration.'
+                    },
+                    {
+                        name: 'User 7',
+                        comment: 'Wow! Nice photos.'
+                    },
+                    {
+                        name: 'User 8',
+                        comment: 'It was a wonderful celebration.'
+                    }
+                ];
+                memory.isMemoryLoading = false;
+
+            }, 3000);
+        };
+
+        if (!memory.comments) {
+            // call service only if comments are not fetched already
+            CallMemoriesForId();
+        } else {
+            memory.isMemoryLoading = false;
+        }
+
+
     }
 }
