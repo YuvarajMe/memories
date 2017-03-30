8d5e871221811f1dfd464546a7db5dd87fdbc355
Yuvaraj <yuvarajcse92@gmail.com>
2017-03-30 23:55:49 +0530

    added validation for login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {$} from "protractor";
 import {sharedService} from "../shared.service";
+import { User} from "../user.interface";
+
 declare const gapi: any;
 
 @Component({
@@ -14,19 +15,32 @@ export class LoginComponent implements OnInit {
 
     public auth2: any;
     // public gapi: any;
+    user: User;
 
-    constructor(private sharedService: sharedService) {
 
+    constructor(public sharedService: sharedService) {
+        this.user = <User>{};
     }
 
     ngOnInit() {
     }
 
     ngAfterViewInit() {
-        this.googleInit();
+        // this.googleInit();
     }
 
-    loggedIn = this.sharedService.isLoggedIn();
+    // get isLoggedIn(): boolean {
+    //     return this.sharedService.isLoggedIn();
+    // }
+
+    public update() {
+        alert('calling update function');
+        this.sharedService.updateLoginStatus(!this.sharedService.isLoggedIn);
+    }
+
+    changeStatus():void {
+        this.sharedService.updateLoginStatus(true);
+    }
 
     public googleInit() {
         let that = this;
@@ -45,7 +59,7 @@ export class LoginComponent implements OnInit {
         let that = this;
         this.auth2.attachClickHandler(element, {},
             function (googleUser) {
-                that.sharedService.updateLoginStatus(true);
+                this.update();
                 let profile = googleUser.getBasicProfile();
                 console.log('Token || ' + googleUser.getAuthResponse().id_token);
                 console.log('ID: ' + profile.getId());
@@ -66,6 +80,8 @@ export class LoginComponent implements OnInit {
             function (googleUser) {
                 var auth2 = gapi.auth2.getAuthInstance();
                 auth2.signOut().then(function () {
+                    that.sharedService.updateLoginStatus(false);
+
                     alert('logout');
                     console.log('User signed out.');
                 });
